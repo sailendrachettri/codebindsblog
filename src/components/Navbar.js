9944dd798a5d13e06ac0b80b8cf7e1b59ac9535c
sailendrachettri <sailendra9083@gmail.com>
2024-03-30 23:37:09 +0530

    collapsable menu added

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,7 +6,7 @@ export const Navbar = () => {
         <>
             <nav class="navbar navbar-expand-lg bg-body-tertiary">
                 <div class="container-fluid">
-                    <Link class="navbar-brand" to="#">Navbar</Link>
+                    <Link class="navbar-brand" to="#">LearnLLM</Link>
                     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span class="navbar-toggler-icon"></span>
                     </button>
@@ -15,25 +15,24 @@ export const Navbar = () => {
                             <li class="nav-item">
                                 <Link class="nav-link active" aria-current="page" to="/">Home</Link>
                             </li>
-                            <li class="nav-item">
-                                <Link class="nav-link" to="#">Link</Link>
-                            </li>
+                            {/* <li class="nav-item">
+                                <Link class="nav-link" to="#"></Link>
+                            </li> */}
                             <li class="nav-item dropdown">
                                 <Link class="nav-link dropdown-toggle" to="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                    Dropdown
+                                    Resources
                                 </Link>
                                 <ul class="dropdown-menu">
-                                    <li><Link class="dropdown-item" to="#">Action</Link></li>
-                                    <li><Link class="dropdown-item" to="#">Another action</Link></li>
+                                    <li><Link class="dropdown-item" to="#">Blogs</Link></li>
+                                    <li><Link class="dropdown-item" to="#">Articles</Link></li>
+                                    <li><Link class="dropdown-item" to="#">Downloads</Link></li>
+                                    <li><Link class="dropdown-item" to="#">Youtube channels</Link></li>
                                     <li><hr class="dropdown-divider" /></li>
-                                    <li><Link class="dropdown-item" to="#">Something else here</Link></li>
+                                    <li><Link class="dropdown-item" to="#">Documentations</Link></li>
                                 </ul>
                             </li>
                         </ul>
-                        <form class="d-flex" role="search">
-                            <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-                            <button class="btn btn-outline-success" type="submit">Search</button>
-                        </form>
+                        <Link to="https://github.com/ParthaPRay/LLM-Learning-Sources" target='_blank' className='text-decoration-none text-dark'>Github</Link>
                     </div>
                 </div>
             </nav>
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -6,25 +6,119 @@ export const Sidebar = () => {
         <>
             <div class="container-fluid col-2 mt-4">
                 <ul class="list-group">
-                    <Link to="#" class="list-group-item active" aria-current="true">Ollama</Link>
-                    <Link to="#" class="list-group-item">Lecture 1 - Setup</Link>
-                    <Link to="#" class="list-group-item">A third item</Link>
-                    <Link to="#" class="list-group-item">A fourth item</Link>
-                    <Link to="#" class="list-group-item">And a fifth one</Link>
-                    <Link to="#" class="list-group-item active">A second item</Link>
-                    <Link to="#" class="list-group-item">A third item</Link>
-                    <Link to="#" class="list-group-item">A fourth item</Link>
-                    <Link to="#" class="list-group-item">And a fifth one</Link>
-                    <Link to="#" class="list-group-item">A second item</Link>
-                    <Link to="#" class="list-group-item">A third item</Link>
-                    <Link to="#" class="list-group-item active">And a fifth one</Link>
-                    <Link to="#" class="list-group-item">A second item</Link>
-                    <Link to="#" class="list-group-item">A third item</Link>
-                    <Link to="#" class="list-group-item">A fourth item</Link>
-                    <Link to="#" class="list-group-item active">And a fifth one</Link>
-                    <Link to="#" class="list-group-item">A second item</Link>
-                    <Link to="#" class="list-group-item">A third item</Link>
-                    <Link to="#" class="list-group-item">A fourth item</Link>
+
+                    {/* // Collaps menu 1 */}
+                    <Link to='#' class="list-group-item active border" data-bs-toggle="collapse" data-bs-target="#collapsmenu-1" aria-expanded="false" aria-controls="collapsmenu-1">
+                        Learn Ollama
+                    </Link>
+                    <div class="collapse" id="collapsmenu-1">
+                        <Link to="https://github.com/ollama/ollama" class="list-group-item">Documentation</Link>
+                        <Link to="https://www.youtube.com/watch?v=xa8pTD16SnM&ab_channel=Decoder" class="list-group-item">Installing Ollama to Customize My Own LLM</Link>
+                        <Link to="https://www.youtube.com/watch?v=syR0fT0rkgY&ab_channel=Decoder" class="list-group-item">Use Your Self-Hosted LLM</Link>
+                        <Link to="https://www.youtube.com/watch?v=fnvZJU5Fj3Q&ab_channel=Decoder" class="list-group-item">Importing Open Source Models to Ollama</Link>
+                    </div>
+
+                    {/* // Collaps menu 2 */}
+                    <Link to='#' class="list-group-item active border" data-bs-toggle="collapse" data-bs-target="#collapsmenu-2" aria-expanded="false" aria-controls="collapsmenu-2">
+                    History of NLP
+                    </Link>
+                    <div class="collapse" id="collapsmenu-2">
+                        <Link to="" class="list-group-item"></Link>
+                        <Link to="" class="list-group-item"></Link>
+                        <Link to="" class="list-group-item"></Link>
+                        <Link to="" class="list-group-item"></Link>
+                    </div>
+
+                    {/* // Collaps menu 3 */}
+                    <Link to='#' class="list-group-item active border" data-bs-toggle="collapse" data-bs-target="#collapsmenu-3" aria-expanded="false" aria-controls="collapsmenu-3">
+                    Large language model
+                    </Link>
+                    <div class="collapse" id="collapsmenu-3">
+                        <Link to="" class="list-group-item"></Link>
+                        <Link to="" class="list-group-item"></Link>
+                        <Link to="" class="list-group-item"></Link>
+                        <Link to="" class="list-group-item"></Link>
+                    </div>
+
+                    {/* // Collaps menu 4 */}
+                    <Link to='#' class="list-group-item active border" data-bs-toggle="collapse" data-bs-target="#collapsmenu-4" aria-expanded="false" aria-controls="collapsmenu-4">
+                    Transformers
+                    </Link>
+                    <div class="collapse" id="collapsmenu-4">
+                        <Link to="" class="list-group-item"></Link>
+                        <Link to="" class="list-group-item"></Link>
+                        <Link to="" class="list-group-item"></Link>
+                        <Link to="" class="list-group-item"></Link>
+                    </div>
+
+                    {/* // Collaps menu 5 */}
+                    <Link to='#' class="list-group-item active border" data-bs-toggle="collapse" data-bs-target="#collapsmenu-5" aria-expanded="false" aria-controls="collapsmenu-5">
+                    Inference Configuration
+                    </Link>
+                    <div class="collapse" id="collapsmenu-5">
+                        <Link to="" class="list-group-item"></Link>
+                        <Link to="" class="list-group-item"></Link>
+                        <Link to="" class="list-group-item"></Link>
+                        <Link to="" class="list-group-item"></Link>
+                    </div>
+
+                    {/* // Collaps menu 6 */}
+                    <Link to='#' class="list-group-item active border" data-bs-toggle="collapse" data-bs-target="#collapsmenu-6" aria-expanded="false" aria-controls="collapsmenu-6">
+                    Fine Tuning
+                    </Link>
+                    <div class="collapse" id="collapsmenu-6">
+                        <Link to="" class="list-group-item"></Link>
+                        <Link to="" class="list-group-item"></Link>
+                        <Link to="" class="list-group-item"></Link>
+                        <Link to="" class="list-group-item"></Link>
+                    </div>
+
+                    {/* // Collaps menu 7 */}
+                    <Link to='#' class="list-group-item active border" data-bs-toggle="collapse" data-bs-target="#collapsmenu-7" aria-expanded="false" aria-controls="collapsmenu-7">
+                    Retrieval augmented generation (RAG)
+                    </Link>
+                    <div class="collapse" id="collapsmenu-7">
+                        <Link to="" class="list-group-item"></Link>
+                        <Link to="" class="list-group-item"></Link>
+                        <Link to="" class="list-group-item"></Link>
+                        <Link to="" class="list-group-item"></Link>
+                    </div>
+
+                    {/* // Collaps menu 8 */}
+                    <Link to='#' class="list-group-item active border" data-bs-toggle="collapse" data-bs-target="#collapsmenu-8" aria-expanded="false" aria-controls="collapsmenu-8">
+                    LLM Applications
+                    </Link>
+                    <div class="collapse" id="collapsmenu-8">
+                        <Link to="" class="list-group-item"></Link>
+                        <Link to="" class="list-group-item"></Link>
+                        <Link to="" class="list-group-item"></Link>
+                        <Link to="" class="list-group-item"></Link>
+                    </div>
+
+                    {/* // Collaps menu 9 */}
+                    <Link to='#' class="list-group-item active border" data-bs-toggle="collapse" data-bs-target="#collapsmenu-9" aria-expanded="false" aria-controls="collapsmenu-9">
+                    Prompt engineering
+                    </Link>
+                    <div class="collapse" id="collapsmenu-9">
+                        <Link to="" class="list-group-item"></Link>
+                        <Link to="" class="list-group-item"></Link>
+                        <Link to="" class="list-group-item"></Link>
+                        <Link to="" class="list-group-item"></Link>
+                    </div>
+                    
+                    {/* // Collaps menu 10 */}
+                    {/* <Link to='#' class="list-group-item active border" data-bs-toggle="collapse" data-bs-target="#collapsmenu-10" aria-expanded="false" aria-controls="collapsmenu-10">
+                    
+                    </Link>
+                    <div class="collapse" id="collapsmenu-10">
+                        <Link to="" class="list-group-item"></Link>
+                        <Link to="" class="list-group-item"></Link>
+                        <Link to="" class="list-group-item"></Link>
+                        <Link to="" class="list-group-item"></Link>
+                    </div> */}
+
+
+
                 </ul>
             </div>
         </>
