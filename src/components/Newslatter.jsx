1b1929e479f2c532445e15b895a46e9ccc1e7179
sailendrachettri <sailendra9083@gmail.com>
2024-04-14 00:28:09 +0530

    form design added for newslatter

--- src/components/Newslatter.jsx
+++ src/components/Newslatter.jsx
@@ -31,7 +31,7 @@ const Newslatter = () => {
         if (response.ok) {
             response.json().then(details => {
                 const user = details.fullname;
-                toast.success(`Thannk you ${user} for submitting your email.`);
+                toast.success(`Thannk you ${user} for subscription.`);
                 setLoading("Submit")
                 setDetails({fullname: "", email: ""});
             })
@@ -46,12 +46,12 @@ const Newslatter = () => {
 
     return (
         <>
-            <form className='login-signup-form' onSubmit={handleSubmit}>
-                <h3 className='text-secondary'>Get latest updates</h3>
+            <form className='container border rounded p-2 my-4' onSubmit={handleSubmit}>
+                <h3 className='text-secondary text-center'>Get latest updates</h3>
                 <div>
-                    <input type="text" id="fullname" name="fullname" placeholder='John Doe' value={details.fullname} onChange={onChange} minLength={3} required autoComplete='off'/>
-                    <input type="email" id="email" name="email" placeholder='doe@john.com' value={details.email} onChange={onChange} required autoComplete='off' />
-                    <button type='submit' onClick={handleLoading}>{loading}</button>
+                    <input type="text" className='form-control my-2' id="fullname" name="fullname" placeholder='John Doe' value={details.fullname} onChange={onChange} minLength={3} required autoComplete='off'/>
+                    <input type="email" className='form-control my-2' id="email" name="email" placeholder='doe@john.com' value={details.email} onChange={onChange} required autoComplete='off' />
+                    <button type='submit' className='btn btn-outline-dark w-100' onClick={handleLoading}>{loading}</button>
                 </div>
             </form>
         </>
