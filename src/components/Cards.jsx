990ea1780ea2d17e2224c44a6594723aad7629f8
sailendrachettri <sailendra9083@gmail.com>
2024-04-15 01:13:42 +0530

    not able add infnite scroll

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -3,8 +3,6 @@ import Card from './Card'
 import { SERVER_URL } from '../environment';
 import Skeleton from 'react-loading-skeleton';
 
-
-
 export const Cards = () => {
     // hooks
     const [posts, setPosts] = useState([]);
@@ -17,7 +15,7 @@ export const Cards = () => {
                 setPosts(posts);
                 setLoading(false);
             });
-        }).catch((err)=>{
+        }).catch((err) => {
             setLoading(false);
             setError(true);
             console.log("Failed to fetch posts");
@@ -25,16 +23,7 @@ export const Cards = () => {
     }, []);
 
 
-    // while loading the content display the skeleton
-    if (loading) return (
-        <div>
-            <Skeleton count={5} width={'80%'}/>
-            <br />
-            <Skeleton count={5} width={'80%'} />
-        </div>
-    )
-
-    if(error) return (
+    if (error) return (
         <div>
             <p className='h3 my-4 text-danger'>Failed to load content! Please refresh the page</p>
             <button className='btn btn-outline-dark'><a href='/' className='text-decoration-none'>Refresh the page</a></button>
@@ -42,17 +31,20 @@ export const Cards = () => {
         </div>
     )
 
-    // if the content is ready the reutrn the content
     return (
         <>
-            <div className='d-flex justify-content-center flex-wrap mt-3'>
-                {
-                    posts.length > 0 && posts.map((post, i) => (
-                        <Card {...post} key={i} />
-                    ))
-                }
+            {!loading ? <div>
+                <div className='d-flex justify-content-center flex-wrap mt-3'>
+                    {
+                        posts.length > 0 && posts.map((post, i) => (
+                            <Card {...post} key={i} />
+                        ))
+                    }
 
-            </div >
+                </div >
+            </div>
+                : <Skeleton count={10} />
+            }
         </>
     )
 }
